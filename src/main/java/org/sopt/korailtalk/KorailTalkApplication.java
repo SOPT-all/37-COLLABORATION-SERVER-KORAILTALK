ef1414621aa5ca91be77ac1c294ea7322052ff8c
seahee <lollol0617@gmail.com>
2025-11-13 22:48:04 +0900

    [#0] setting: 프로젝트 초기세팅

---
+++ src/main/java/org/sopt/korailtalk/KorailTalkApplication.java
@@ -0,0 +1,11 @@
+package org.sopt.korailtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorailTalkApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KorailTalkApplication.class, args);
+	}
+}
