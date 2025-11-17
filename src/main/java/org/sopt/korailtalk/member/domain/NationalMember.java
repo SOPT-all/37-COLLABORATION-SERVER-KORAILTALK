236815f32e6a0e44ddd904e3e282ce515edfd3e1
Seoyoung Cho <bera1125@naver.com>
2025-11-17 16:19:41 +0900

    feat: member 패키지 도메인 설정

---
+++ src/main/java/org/sopt/korailtalk/member/domain/NationalMember.java
@@ -0,0 +1,33 @@
+package org.sopt.korailtalk.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.korailtalk.global.entity.BaseTimeEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "national_members")
+public class NationalMember extends BaseTimeEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "national_member_id")
+  private Long id;
+
+  @Column(unique = true, nullable = false)
+  private String nationalId;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private LocalDateTime birthdate;
+}
