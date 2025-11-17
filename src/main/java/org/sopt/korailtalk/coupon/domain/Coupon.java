0c3cf987cc4d88101f16866471b8a3a14e5fcc3e
Seoyoung Cho <bera1125@naver.com>
2025-11-17 16:22:40 +0900

    feat: coupon 패키지 도메인 설정

---
+++ src/main/java/org/sopt/korailtalk/coupon/domain/Coupon.java
@@ -0,0 +1,26 @@
+package org.sopt.korailtalk.coupon.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "coupons")
+public class Coupon {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "coupon_id")
+  private Long id;
+
+  private String name;
+  private Integer discountRate;
+}
