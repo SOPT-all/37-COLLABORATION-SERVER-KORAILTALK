d196639e70eeed15233297c70ca64642082229f6
seahee <lollol0617@gmail.com>
2025-11-19 19:53:00 +0900

    [#5] refactor(ReservationQueueRepository): 만료된 예약 데이터를 가져올 때 train을 entityGraph로 함께 가져옴

--- src/main/java/org/sopt/korailtalk/reservation/repository/ReservationQueueRepository.java
+++ src/main/java/org/sopt/korailtalk/reservation/repository/ReservationQueueRepository.java
@@ -4,9 +4,11 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.sopt.korailtalk.reservation.domain.ReservationQueue;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationQueueRepository extends JpaRepository<ReservationQueue, Long> {
 
+	@EntityGraph(attributePaths = {"train"})
 	List<ReservationQueue> findByExpireAtBefore(LocalDateTime now);
 }
