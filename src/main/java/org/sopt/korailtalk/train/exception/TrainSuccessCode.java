b1c157867c058d9da98da3a8248b02f718685693
JJhwan7099 <ghks7099@naver.com>
2025-11-23 07:15:53 +0900

    fix: merge 충돌 해결

--- src/main/java/org/sopt/korailtalk/train/exception/TrainSuccessCode.java
+++ src/main/java/org/sopt/korailtalk/train/exception/TrainSuccessCode.java
@@ -1,6 +1,6 @@
 package org.sopt.korailtalk.train.exception;
 
-import org.sopt.korailtalk.global.response.base.BaseSuccessCode;
+import org.sopt.korailtalk.global.response.base.BaseCode;
 import org.springframework.http.HttpStatus;
 
 import lombok.Getter;
