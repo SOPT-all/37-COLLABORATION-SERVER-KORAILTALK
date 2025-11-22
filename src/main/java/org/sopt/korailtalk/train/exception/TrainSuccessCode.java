8e80cdd395947484fe324af797c955b97ce66698
JJhwan7099 <ghks7099@naver.com>
2025-11-22 23:05:38 +0900

    refactor: 열차 조회 성공 enum 200인 경우로 변경

--- src/main/java/org/sopt/korailtalk/train/exception/TrainSuccessCode.java
+++ src/main/java/org/sopt/korailtalk/train/exception/TrainSuccessCode.java
@@ -11,10 +11,10 @@ import lombok.RequiredArgsConstructor;
 public enum TrainSuccessCode implements BaseSuccessCode {
 
 	//200 OK
+	GET_TRAINS_SUCCESS(HttpStatus.OK, "열차 목록 조회 성공"),
 
 	//201 CREATED
-	GET_TRAIN_INFO_SUCCESS(HttpStatus.CREATED, "열차 정보 조회 및 예약 대기 처리가 성공하였습니다."),
-	GET_TRAINS_SUCCESS(HttpStatus.OK, "열차 목록 조회 성공");
+	GET_TRAIN_INFO_SUCCESS(HttpStatus.CREATED, "열차 정보 조회 및 예약 대기 처리가 성공하였습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
